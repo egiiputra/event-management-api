20b97c9a843d37379eac56599a0bf49022d6223a
egiiputra <egiputra450@gmail.com>
2025-05-30 22:04:54 +0700

    add handler for adding event data

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,11 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
+import { Event } from './entities/event.entity'
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Ticket } from 'src/tickets/entities/ticket.entity';
+import { randomUUID } from 'node:crypto'
 
 @Injectable()
 export class EventsService {
-  create(createEventDto: CreateEventDto) {
-    return 'This action adds a new event';
+  constructor(
+    @InjectRepository(Event)
+    private readonly eventsRepository: Repository<Event>,
+    @InjectRepository(Ticket)
+    private readonly ticketsRepository: Repository<Ticket>
+  ) {}
+
+  async create(createEventDto: CreateEventDto) {
+    const event = new Event()
+
+    event.id = randomUUID()
+    event.name = createEventDto.name
+    event.description = createEventDto.description
+    event.start_date_time = createEventDto.start_date_time
+    event.end_date_time = createEventDto.end_date_time
+    event.is_online = createEventDto.is_online
+
+    await this.eventsRepository.save(event)
+
+    createEventDto.tickets.forEach(async (ticket) => {
+      const tmp = new Ticket()
+
+      tmp.qty = ticket.qty
+      tmp.price = ticket.price
+      tmp.start_sale = ticket.start_sale
+      tmp.end_sale = ticket.end_sale
+      tmp.event = event
+
+      await this.ticketsRepository.save(tmp)
+    })
+
+    return 'berhasil'
   }
 
   findAll() {
