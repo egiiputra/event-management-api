e3509a01c3af0ae8cefc4c3c14bd15a81c824654
egiiputra <egiputra450@gmail.com>
2025-05-30 22:04:19 +0700

    setup typeorm for event and ticket entity

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule} from '@nestjs/typeorm'
 import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
+import { Event } from './entities/event.entity'
+import { Ticket } from 'src/tickets/entities/ticket.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Event]),
+    TypeOrmModule.forFeature([Ticket]),
+  ],
   controllers: [EventsController],
   providers: [EventsService],
 })
